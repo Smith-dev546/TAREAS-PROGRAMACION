16782ad6b48cbbd3e1fbce8eeab631f71bd4d932
Smith-dev546 <smithmendez098@gmail.com>
2025-07-29 21:39:12 -0600

    Agregando ejercicios de readline al repositorio

---
+++ MODULOS/Ejercicio.js
@@ -0,0 +1,42 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+/*
+    Ejemplo
+
+rl.question('Cuál es tu nombre? ', (nombre) => {
+    console.log(`Hola ${nombre}, Cómo estás?`)
+    rl.close();
+
+
+*/
+
+/*
+    //Ejercicio 1
+
+    rl.question('En que ciudad vives? ', (ciudad) => {
+        console.log(`${ciudad} es una ciudad muy bonita!`);
+        rl.close();
+
+*/
+
+/*
+//Ejercicio 2
+
+    rl.question('Cuál es tu color favorito? ', (color) => {
+    console.log(` tu color favorito es ${color}.`);
+    rl.close();
+
+    */
+
+    // Ejercicio 3
+    rl.question('Cuál es tu nombre? ', (nombre) => {
+        rl.question('Cuál es tu apellido? ', (apellido) => {
+    
+    console.log(`Hola ${nombre} ${apellido}`);
+    rl.close();
+        });
+    })
