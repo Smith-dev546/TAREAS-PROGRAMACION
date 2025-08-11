5d088112e1f74766f9cab342f95fda86ae8ea4be
Smith-dev546 <smithmendez098@gmail.com>
2025-08-11 10:24:13 -0600

    Agregndo clase 11/08/2025 metodos de js

---
+++ MODULOS/Clase-11-08-25/Metodos.js
@@ -0,0 +1,75 @@
+//Importamos el modulo
+import readline from 'readline';
+
+//Creamos el interfaz de lectura
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+//Realizamos la pregunta al usuario
+rl.question('Ingrese una frase: ', (frase) =>{
+    console.log('\n === Tratamiento de Cadenas ===');
+
+    // 1. length    => Nos devuelve la longitud de la cadena
+    
+    const fraseLength = frase.length;
+    console.log(`La longitud de la frase es: ${fraseLength}`);
+
+    // 2. toUpperCase() => Convierte la cadena a mayusculas
+    const fraseMayus = frase.toUpperCase();
+    console.log(`La frase en mayusculas es: ${fraseMayus}`);
+
+    // 3. toLowerCase() => Convierte la cadena a minusculas
+   const fraseMinus = frase.toLowerCase();
+   console.log(`La frase en minusculas es: ${fraseMinus}`);
+
+   // 4. trim() => Elimina los espacios en blanco al inicio y al final
+   const fraseTrim = frase.trim();
+   console.log(`La frase sin espacios es: ${fraseTrim}`);
+
+   // 5. substring() => Extrae una parte de la cadena
+   const fraseSub = frase.substring(0, 5);
+   console.log(`Los primeros 5 caracteres de la frase son: ${fraseSub}`);
+
+   // 6. slice() => Extrae una parte de la cadena
+   const fraseSlice = frase.slice(0, 5);
+   console.log(`Los primeros 5 caracteres de la frase son: ${fraseSlice}`);
+
+   //Este si toma valores negativos
+   const subCadenasSlice = frase.slice(-5);
+   console.log(`Los ultimos 5 caracteres de la frase son: ${subCadenasSlice}`);
+
+   //7. replace
+   const fraseReplace = frase.replace('hola', 'adios');
+   console.log(`La frase con 'hola' reemplazado por 'adios' es: ${fraseReplace}`);
+
+   // 8. replaceAll() => Reemplaza todas las ocurrencias de una cadena
+   const fraseReplaceAll = frase.replaceAll('hola', 'adios');
+   console.log(`La frase con todas las ocurrencias de 'hola' reemplazadas por 'adios' es: ${fraseReplaceAll}`);
+
+   // 9. split() => Divide la frase en plabras separandolas por un espacio
+   const fraseAray = frase.split(' ');
+   console.log(`La frase dividida en palabras es: ${fraseAray}`);
+
+   //10. includes() => Determina si una cadena contiene otra cadena
+   const fraseIncluyeHola = frase.includes('hola');
+   console.log(`La frase incluye la palabra 'hola': ${fraseIncluyeHola}`);
+
+   //11. indexOf() => Devuelve la posición de la primera ocurrencia de una cadena
+   const IndiceHola = frase.indexOf('hola');
+   console.log(`El indice de la primera ocurrencia de 'hola' es: ${IndiceHola}`);
+
+   // 12. startsWith() => Determina si una cadena comienza con otra cadena
+   const fraseEmpiezaConHola = frase.startsWith('hola');
+   console.log(`La frase empieza con 'hola': ${fraseEmpiezaConHola}`);
+
+   //13. endsWith() => Determina si una cadena termina con otra cadena
+   const fraseTerminaConAdios = frase.endsWith('adios');
+   console.log(`La frase termina con 'adios': ${fraseTerminaConAdios}`);
+
+    //Cerramos el interfaz
+rl.close();
+
+});
+
